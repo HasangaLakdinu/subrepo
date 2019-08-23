c9a595b593dc88be61fa70468e74a0f96d769e54
Hasanga Lakdinu <hasangalakdinu@gmail.com>
2019-08-23 09:42:48 +0530

    add another from sub

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ console.log("This is sub repo index.js");
 console.log("And this is direct from sub repo ");
 console.log("And this is direct from sub repo this is second one ");
 
+
+console.log("Hello hasanga this is from sub again ");
+
