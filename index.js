2e6bfb6a1aecc1d9b06d9b17d5ab84ea26e67856
Hasanga Lakdinu <hasangalakdinu@gmail.com>
2019-08-23 09:36:00 +0530

    add another line from master

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("This is sub repo index.js");
 console.log("And this is direct from sub repo ");
 console.log("And this is direct from sub repo this is second one ");
 
+console.log("This is from main repo");
